cd33bd28ad95317d067fe3355a471ac1039f353b
anadhima <a.dhima.gr@gmail.com>
2021-04-09 09:38:37 +0100

    Foreground and Background

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -24,11 +24,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
+import com.google.firebase.messaging.FirebaseMessaging
 
 class EggTimerFragment : Fragment() {
 
@@ -57,15 +59,14 @@ class EggTimerFragment : Fragment() {
 
         // TODO: Step 3.1 create a new channel for FCM
         createChannel(
-            getString(R.string.egg_notification_channel_id),
-        getString(R.string.breakfast_notification_channel_name)
+            getString(R.string.breakfast_notification_channel_id),
+            getString(R.string.breakfast_notification_channel_name)
         )
 
 
-
+        subscribeTopic()
         return binding.root
     }
-
 
 
     private fun createChannel(channelId: String, channelName: String) {
@@ -77,7 +78,7 @@ class EggTimerFragment : Fragment() {
                 // TODO: Step 2.4 change importance
                 NotificationManager.IMPORTANCE_HIGH
             )
-            // TODO: Step 2.6 disable badges for this channel
+                // TODO: Step 2.6 disable badges for this channel
                 .apply {
                     setShowBadge(false)
                 }
@@ -97,7 +98,22 @@ class EggTimerFragment : Fragment() {
 
     }
 
-        companion object {
+    // TODO: Step 3.3 subscribe to breakfast topic
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
+    }
+
+
+    companion object {
         fun newInstance() = EggTimerFragment()
     }
 }
